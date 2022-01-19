436ebf37bced1eb40a027e57af8bd85e411d5405
skimmy44 <jjswhappy@gmail.com>
2022-01-19 00:54:19 -0800

    edit

--- Easy/RomanToInteger.py
+++ Easy/RomanToInteger.py
@@ -1,5 +1,5 @@
 class Solution:
-    def romanToInt(self, s: str) -> int:
+    def romanToInt(self, s: str) -> int: #keys are always string
         romanHash = {'I':1,
                     'V':5,
                     'X':10,
@@ -8,15 +8,10 @@ class Solution:
                     'D':500,
                     'M':1000}
         reVal = 0
-        
-        for i in range(0, len(s)-1):
-            #"MCMXCIV" = 1994 -> M=1000, CM=900, XC=90, IV=4
+        for i in range(0, len(s)-1): #i는 0부터 number before len(s)-1 까지 #"MCMXCIV" = 1994 -> M=1000, CM=900, XC=90, IV=4
             if (romanHash[s[i]] < romanHash[s[i+1]]):
                 reVal = reVal - romanHash[s[i]]
-                #if the value of character of current iteration is smaller than 
-                #the next value of character, substract it into reVal (!!!!)
+                #if the value of character of current iteration is smaller than  #the next value of character, substract it into reVal (!!!!)
             else:
-                reVal = reVal + romanHash[s[i]]
-                #otherwise, add that number into reVal
-
+                reVal = reVal + romanHash[s[i]] #otherwise, add that number into reVal 
         return reVal + romanHash[s[-1]] #return the reVal plus the value of last Roman Character
