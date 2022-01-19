2d5b3e82a331f1a140dfb3265f57e8beea53bf23
skimmy44 <jjswhappy@gmail.com>
2022-01-18 23:06:53 -0800

    2022/01/18 Roman To Integer

---
+++ Easy/RomanToInteger.py
@@ -0,0 +1,22 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romanHash = {'I':1,
+                    'V':5,
+                    'X':10,
+                    'L':50,
+                    'C':100,
+                    'D':500,
+                    'M':1000}
+        reVal = 0
+        
+        for i in range(0, len(s)-1):
+            #"MCMXCIV" = 1994 -> M=1000, CM=900, XC=90, IV=4
+            if (romanHash[s[i]] < romanHash[s[i+1]]):
+                reVal = reVal - romanHash[s[i]]
+                #if the value of character of current iteration is smaller than 
+                #the next value of character, substract it into reVal (!!!!)
+            else:
+                reVal = reVal + romanHash[s[i]]
+                #otherwise, add that number into reVal
+
+        return reVal + romanHash[s[-1]] #return the reVal plus the value of last Roman Character
