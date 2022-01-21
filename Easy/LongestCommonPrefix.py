7de844cf628180f66ff0a352ca5155623efefacc
skimmy44 <jjswhappy@gmail.com>
2022-01-20 19:53:58 -0800

    yesterday's question

--- Easy/LongestCommonPrefix.py
+++ Easy/LongestCommonPrefix.py
@@ -1,8 +1,18 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        dict = {}
-        for i, val in enumerate(strs):
-            dict[val] = i
+        shortest = strs[0]
+        if len(strs) == 0: # if empty, return ""
+            return ""
+        elif len(strs) == 1: # if one string contained, return that string
+            return shortest
+
+        for i, val in enumerate(strs): # find the shortest length of string in strs
+            if len(shortest) > len(val):
+                shortest = val
         
-        reVal = ""
+        for i, val in enumerate(shortest): # find if char of same index match, 
+            for j in strs:
+                if (shortest[i] != j[i]):
+                    return shortest[:i]  # otherwise, return the string without the character of current iteration
+        return shortest
         
